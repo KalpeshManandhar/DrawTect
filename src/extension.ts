6c24b61c195e6ee7360e68c486a6f7af579db154
Mavis021 <077BCT043.mamata@pcampus.edu.np>
2024-04-17 16:25:07 +0545

    activates providers registration after selection of comment that includes specified text

--- src/extension.ts
+++ src/extension.ts
@@ -8,11 +8,16 @@ import { DT_CodeActionsProvider } from './DrawTectCodeActions';
 export function activate(context: vscode.ExtensionContext) {
 	
 
-	// Register our custom editor providers
-	context.subscriptions.push(DT_EditorProvider.register(context));
-	context.subscriptions.push(DT_CodeActionsProvider.register(context));
-	
-	console.log("Provider registered");
+    vscode.window.onDidChangeTextEditorSelection(e => {
+        const text = e.textEditor.document.getText(e.selections[0]);
+        if (text.includes('dt:')|| text.includes("drawtect:")) {
+            // Register our custom editor providers
+            context.subscriptions.push(DT_EditorProvider.register(context));
+            context.subscriptions.push(DT_CodeActionsProvider.register(context));
+
+            console.log("Provider registered");
+        }
+    });
 
 	// sendDataForHTR([]);
 
